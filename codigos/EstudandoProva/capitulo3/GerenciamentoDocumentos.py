74007d138957959907b85861d5a6e11611b89a7e
IsabelleRancan <isabellerancan@gmail.com>
2023-10-03 22:51:03 -0400

    exercicio finalizado
    exercicio 1 do capitulo 3 finalizado

--- codigos/EstudandoProva/capitulo3/GerenciamentoDocumentos.py
+++ codigos/EstudandoProva/capitulo3/GerenciamentoDocumentos.py
@@ -14,40 +14,64 @@ class Documentos(abc.ABC):
         "método de medidas que estará presente em todas as classes, fazer mais tarde"
         pass
 
-    def dimensoes(self, dimensoes):
-        "método que retornará a quantidade de pixels se um documento tievr dimensões, fazer mais tarde"
-        pass
 
 class Pdf(Documentos):
     "nesse método não preciso mais pegar nenhum atributo novo"
     
     def __init__(self, autor, data, tamanho):
         super().__init__(autor,data, tamanho)
-        print (f"Especificações sobre o documento {self}.")
+        print ("----------------------------------------------------")
+        print (f"Especificações sobre o documento PDF:")
         print (f"Autor: {self.autor}.")
-        print (f"Data {self.data}.")
-        print (f"Tamanho {self.tamanho}.")
+        print (f"Data: {self.data}.")
+        print (f"Tamanho: {self.tamanho}.")
 
 class DocsWord(Documentos):
     "nesse método não preciso mais pegar nenhum atributo novo"
     
     def __init__(self, autor, data, tamanho):
         super().__init__(autor,data, tamanho)
+        print ("----------------------------------------------------")
+        print (f"Especificações sobre o documento Word:")
+        print (f"Autor: {self.autor}.")
+        print (f"Data: {self.data}.")
+        print (f"Tamanho: {self.tamanho}.")
+
 
 class Imagem(Documentos):
-    "nesse método estou adicionando o parametro dimensoes"
+    "nesse método estou adicionando o metodo dimensoes"
     
-    def __init__(self, autor, data, tamanho, dimensoes):
-        self.dimensoes = dimensoes
+    def __init__(self, autor, data, tamanho, altura, largura):
         super().__init__(autor,data, tamanho)
+        self.altura=altura
+        self.largura=largura
+        print ("----------------------------------------------------")
+        print (f"Especificações sobre o documento: ")
+        print (f"Autor: {self.autor}.")
+        print (f"Data: {self.data}.")
+        print (f"Tamanho: {self.tamanho}.")
+        print (f"Pixels: {self.dimensoes()}.")
 
-class Video(Documentos):
-    "nesse método estou adicionando dimencoes e a duração"
+
+    def dimensoes(self):
+        return self.altura * self.largura
+
+class Video(Imagem):
+    "essa classe herda imagem para podermos manipular os atributos no método dimensoes"
     
-    def __init__(self, autor, data, tamanho, dimensoes, duracao):
-        self.dimensoes = dimensoes
+    def __init__(self, autor, data, tamanho, altura, largura, duracao):
         self.duracao = duracao
-        super().__init__(autor,data, tamanho)
+        super().__init__(autor, data, tamanho, altura, largura)
+        print (f"Duração: {self.duracao}.")
+
+    def dimensoes(self):
+        return super().dimensoes()
 
 teste = Pdf("ana", "06/05/2004", 5)
+print()
+
+teste2 = Imagem("isa", "06/05/2004", 5, 10, 5)
+print()
+
+teste3 = Video("maria", "20/05/1975", 2, 20, 10, 120)
 print()
