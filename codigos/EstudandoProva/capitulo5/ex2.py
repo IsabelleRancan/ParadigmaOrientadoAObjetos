02b6c2032f5a65aa474a2d7ecd8aa7d8f3056acc
IsabelleRancan <isabellerancan@gmail.com>
2023-12-04 00:44:33 -0400

    commit de domingo a noite

---
+++ codigos/EstudandoProva/capitulo5/ex2.py
@@ -0,0 +1,21 @@
+import math
+
+class Math:
+    """Criando a classe Math e o atributo de classe precision.
+    O atributo precision é o determinante do número de casas decimais após a vírgula que
+    o código vai retornar."""
+    precision = 5
+
+    @property
+    def pi(self):
+        """Criando a propriedade de classe pi.
+        Essa classe vai retornar o valor de pi com o número determinado de casas decimais."""
+        formato = "{:." + str(self.precision) + "f}"  # Acessando o atributo de instância precision e definindo o formato
+        return formato.format(math.pi)
+
+# Criando uma instância da classe Math
+minha_classe_math = Math()
+
+# Acessando o método pi, formatando e imprimindo o valor
+valor_formatado = minha_classe_math.pi
+print("Valor de pi formatado:", valor_formatado)
